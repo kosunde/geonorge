10330e0d17dd78330aa77d9764313b3b41e9ee32
Knut Olav Sunde <knut.olav.sunde@outlook.com>
2015-09-07 15:06:26 +0200

    Planomriss SQL'er for views til WFS

---
+++ Arealplaner/Planomriss_postgis2wfs/views/v_kpgrense.sql
@@ -0,0 +1,9 @@
+﻿create or replace view planomriss.v_kpgrense as
+select kpgrense.objtype,
+    kpgrense.grense,
+    sosi_objekt_planlinjer_kp.forstedigitaliseringsdato,
+    sosi_objekt_planlinjer_kp.noyaktighet,
+    sosi_objekt_planlinjer_kp.synbarhet,
+    sosi_objekt_planlinjer_kp.oppdateringsdato
+   from planomriss.kpgrense
+     join planomriss.sosi_objekt_planlinjer_kp on kpgrense.objid = sosi_objekt_planlinjer_kp.kpgrense_fk;
