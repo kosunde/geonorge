3a08f7b604e94e26ebc6bda45bf1ae600943f538
kosunde <knut.olav.sunde@outlook.com>
2015-10-27 15:41:42 +0100

    Vertikalbilder - enda et view

---
+++ Vertikalbilder/Views/v_vertikalbildedekning.sql
@@ -0,0 +1,25 @@
+﻿CREATE OR REPLACE VIEW vertikalbilde.v_vertikalbildedekning AS 
+ SELECT dekning.objid,
+  dekning.objtype,
+  dekning.oppdragsnavn,
+  dekning.dekningsnummer,
+  dekning.bakkeopplosning,
+  dekning.bildefilformat,
+  dekning.bildemalestokk,
+  dekning.bitsprpixel,
+  dekning.flyfirma,
+  dekning.oppdragsgiver,
+  dekning.aerotriangulering,
+  dekning.orienteringsdata,
+  dekning.orienteringsmetode,
+  dekning.lengdeoverlapp,
+  dekning.sideoverlapp,
+  dekning.prosjektrapportlink,
+  dekning.skanneropplosning,
+  dekning.omrade,  
+  fotodato.fotodato,
+  fotodato.vertikalbildedekningfk
+FROM 
+  vertikalbilde.vertikalbildedekning dekning, 
+  (SELECT DISTINCT ON (vertikalbildedekningfk) * FROM vertikalbilde.fotodato) fotodato
+WHERE fotodato.vertikalbildedekningfk = dekning.objid;
